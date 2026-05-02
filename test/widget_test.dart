668270095d28b5d0b85525a961b9527e285bc202
JedSebios <sebiosjed@gmail.com>
2026-05-02 02:08:04 +0200

    Fix loading state widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:woodpecker_chess/data/repositories/puzzle_repository.dart';
+import 'package:woodpecker_chess/features/solve/puzzle.dart';
 import 'package:woodpecker_chess/features/solve/solve_screen.dart';
 
 void main() {
   testWidgets('SolveScreen shows loading state when puzzle is not yet ready',
       (tester) async {
+    final pendingPuzzle = Completer<Puzzle>();
     await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(home: SolveScreen()),
+      ProviderScope(
+        overrides: [
+          eloRandomPuzzleProvider.overrideWith((ref) => pendingPuzzle.future),
+        ],
+        child: const MaterialApp(home: SolveScreen()),
       ),
     );
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
